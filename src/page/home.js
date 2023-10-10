ce160f6b6b3103dc6ae2c66a49a7dd2aed594aac
Larissa Stack <larissastack@gmail.com>
2023-10-10 14:25:14 -0700

    home page

---
+++ src/page/home.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+        <div>
+            <h1>Lady Panther Basketball</h1>
+            <br></br>
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="..." class="d-block w-100" alt="..."/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="..."/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="..."/>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            <h2>Who We Are</h2>
+            <p>A paragraph about who we are will go here. </p>
+            <br></br>
+            <h2>Our Goals</h2>
+            <p>What our goal are. </p>
+            <br></br>
+            <h2>Our Schedule</h2>
+            <p>Our schedule will go here. </p>
+            <br></br>
+            <h2>Our Coaches</h2>
+            <p>Our coaches will go here. </p>
+        </div>
+        </>
+    );
+};
+
+export default Home;
