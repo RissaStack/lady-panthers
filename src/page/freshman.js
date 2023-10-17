2cb3e104cd8fcbcdc88cb96161ffb738cc3cf782
Larissa Stack <larissastack@gmail.com>
2023-10-17 13:59:44 -0700

    fixed an error on the freshman page

---
+++ src/page/freshman.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  {freshPlayers} from '../data/';
+import Team from '../components/Team';
+
+const Freshman = () => {
+  return (
+  <div className="row row-cols-1 row-cols-md-2 rwo-col-xl-3 g-4">
+    {freshPlayers.map((team, i) => (
+      <Team team={team} key={i} />
+    ))}
+    </div>
+  );
+};
+
+export default Freshman;
